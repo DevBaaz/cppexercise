f8446ada8a7e280df9cc5cd1c875ea5fff49a8d5
DevBaaz <adekunleabdulazeez003@gmail.com>
2023-11-12 16:38:32 +0100

    first commit

---
+++ c01exercise.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    //exercise 1-1: Create, compile, link, and execute a program that will display the text "Hello World" on your screen.
+    std::cout << "\"Hello World\"" << std::endl;
+
+    //exercise 1-2: Create and execute a program that outputs your name on one line and your age on the next line.
+    std::cout << "Adekunle Abdul-Azeeez \n 24" << std::endl;
+
+    /*
+    exercise 1-3: the following program produces several compiler errors. Find these
+    errors and correct them so the program can compile cleanly and run.
+    #import <iostream>
+    Int main
+    {
+    std:cout << "Hola Mundo!" << std:endl
+    )
+    */ 
+   std::cout << "Hola Mundo!"  << std::endl;
+}
